8aefcf8ebf19331e5ccd6f3470aeecb5c6252b7b
Drew Karriker <Drew@drewlearns.com>
2020-02-05 00:32:54 -0500

    create card-list component

---
+++ monsters/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card-list.styles.css'
+
+export const CardList = props => {
+        console.log(props);
+        {this.state.monsters.map(monster => (
+                <h1 key={ monster.id} > { monster.name } </h1>
+              ))}
+};
