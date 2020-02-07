98694c17690ea552c6671223f45ec983e0243cd4
Drew Karriker <Drew@drewlearns.com>
2020-02-06 19:58:03 -0500

    Updates

---
+++ precache-manifest.1c2f5ec94c215ba91760b91fbf752451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179b6d8a59de6de3616ae786ecbf5065",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fdacc5c93e8e2d2e095",
+    "url": "/monsters-rolodex/static/css/main.602a1e00.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdacc5c93e8e2d2e095",
+    "url": "/monsters-rolodex/static/js/main.cfdd6f2b.chunk.js"
+  },
+  {
+    "revision": "779c1edafb9d6ee57b27",
+    "url": "/monsters-rolodex/static/js/runtime-main.094e9a2b.js"
+  }
+]);
